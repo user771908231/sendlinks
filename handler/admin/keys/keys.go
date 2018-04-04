a7f4f43a2adef3c43647f532534be75246113b1b
user771908231 <771908231@qq.com>
2018-04-04 22:28:38 +0800

    one

---
+++ handler/admin/keys/keys.go
@@ -0,0 +1,25 @@
+package keys
+
+import "sendlinks/modules"
+
+func IndexHandler(ctx *modules.Context) {
+
+}
+
+
+func AddHandler(ctx *modules.Context) {
+
+}
+
+func DeleteHandler(ctx *modules.Context) {
+
+}
+
+
+func SelectHandler(ctx *modules.Context) {
+
+}
+
+func UplateHandler(ctx *modules.Context) {
+
+}
---
+++ handler/admin/links/links.go
@@ -0,0 +1,26 @@
+package links
+
+import "sendlinks/modules"
+
+func IndexHandler(ctx *modules.Context) {
+	ctx.HTML(200,"/x-admin/links/index")
+}
+
+
+func AddHandler(ctx *modules.Context) {
+
+}
+
+func DeleteHandler(ctx *modules.Context) {
+
+}
+
+
+func SelectHandler(ctx *modules.Context) {
+
+}
+
+func UplateHandler(ctx *modules.Context) {
+
+}
+
